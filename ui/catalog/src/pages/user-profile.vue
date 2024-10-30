508b9885cd7a7bfb5f918cb2f2565ba1c6c50331
Viktor <vikkes@gmail.com>
2024-10-30 06:12:29 -0700

    user profile

--- ui/catalog/src/pages/user-profile.vue
+++ ui/catalog/src/pages/user-profile.vue
@@ -1,7 +1,17 @@
 <template>
-  <div>Name: {{ user.given_name }} {{ user.family_name }}</div>
-
-  <div>Email: {{ user.email }}</div>
+  <v-row>
+    <v-col cols="6">
+      <v-card class="ma-6">
+        <v-list-item three-line class="mb-12">
+          <div class="text-overline mb-4">User Profile</div>
+          <v-list-item-title class="text-h5 mb-1">
+            {{ user.given_name }} {{ user.family_name }}
+          </v-list-item-title>
+          <v-list-item-subtitle>G{{ user.email }}</v-list-item-subtitle>
+        </v-list-item>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script lang="ts" setup>
